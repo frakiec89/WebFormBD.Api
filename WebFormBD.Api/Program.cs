c8d68db58273762f72001705f1b6654fdeb37f6a
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-01-11 16:49:04 +0400

    добавил Апи конфиг

--- WebFormBD.Api/Program.cs
+++ WebFormBD.Api/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,7 +22,31 @@ namespace WebFormBD.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls("http://192.168.10.160:5501;");
+                    webBuilder.UseStartup<Startup>().UseUrls(GetURL().Result);
                 });
+
+
+
+        private static async Task<string> GetURL()
+        {
+            if (!File.Exists("configApi.json"))
+                throw new Exception("Файл configApi.json отсутствует");
+
+            try
+            {
+                var s = "configApi.json";
+
+                using (StreamReader r = new StreamReader(s))
+                {
+                    string json = await r.ReadToEndAsync();
+                    var config = JsonConvert.DeserializeObject<dynamic>(json);
+                    return $"{config.protokol}://{config.url}:{config.port};";
+                }
+            }
+            catch
+            {
+                throw new Exception("Файл configApi.json поврежден");
+            }
+        }
     }
 }
